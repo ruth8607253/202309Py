3603b41057582012fe8469c4cb67a0e64d31d865
ruth <131242482+ruth8607253@users.noreply.github.com>
2023-09-26 14:14:42 +0800

    筆記

--- 總綱/模組/跟命令有關的模組.py
+++ 總綱/模組/跟命令有關的模組.py
@@ -41,12 +41,3 @@ import sys
 import twstock
 #在終端機打$ twstock -s 0050(或其他股票代號，可以1~多個同時輸入)
 #在終端機打$ twstock -b 0050(或其他股票代號，可以1~多個同時輸入)：判斷是否適合買入
-
-#分析計算
-from twstock import Stock
-
-stock = Stock('2330')                             # 擷取台積電股價
-ma_p = stock.moving_average(stock.price, 5)       # 計算五日均價
-ma_c = stock.moving_average(stock.capacity, 5)    # 計算五日均量
-ma_p_cont = stock.continuous(ma_p)                # 計算五日均價持續天數
-ma_br = stock.ma_bias_ratio(5, 10)                # 計算五日、十日乖離值
