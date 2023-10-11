36e75280b341695dae6f0fa6c3e3cc0ab3c7ff13
ruth <131242482+ruth8607253@users.noreply.github.com>
2023-10-11 17:29:57 +0800

    筆記

--- 20231005作業/上課內容/datasource.py
+++ 20231005作業/上課內容/datasource.py
@@ -2,6 +2,8 @@ import requests
 import csv
 import io
 
+__cities=[]
+
 def __download()->list[list]:
     # 111年台灣各縣市人口密度
     url="https://data.moi.gov.tw/MoiOD/System/DownloadFile.aspx?DATA=CA18EE06-4A50-4861-9D97-7853353D7108"
@@ -22,13 +24,30 @@ def __download()->list[list]:
             return list(a_reader)
 
 def cities_info()->list[list]:
-    cities=[]
-    try:
-        a_list=__download()
-    except Exception as i:
-        print(f"錯誤：{i}")
+    if len(__cities)==0: 
+        try:
+            a_list=__download()
+        except Exception as i:
+            print(f"錯誤：{i}")
+        else:
+            for i in a_list:
+                if i[0] =='111':
+                    __cities.append(i)
+        return __cities
     else:
-        for i in a_list:
-            if i[0] =='111':
-                cities.append(i)
-    return cities
+        return __cities
+    
+def city_names()->list[str]:
+    cities=cities_info()
+    name=[]
+    for i in cities:
+        cityname=i[1]
+        name.append(cityname)
+    return name
+
+def info(name:str)->list[str]:
+    cities=cities_info()
+    for i in cities:
+        if i[1] == name:
+            return i
+    return []
